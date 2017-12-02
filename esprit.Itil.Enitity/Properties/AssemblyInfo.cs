157ef7bdc59a06264173a90f92c275c3cf580749
fksontini <fksontini@SP2016>
2017-12-02 15:43:21 +0100

    Add project files.

---
+++ esprit.Itil.Enitity/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("esprit.Itil.Enitity")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("esprit.Itil.Enitity")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1d4a94e5-055b-490c-9764-fb16fda9e160")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ esprit.Itil.SharePoint/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="5f02186b-d9ba-4df0-875a-15534d661778" featureId="5f02186b-d9ba-4df0-875a-15534d661778" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="esprit.Itil.SharePoint Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="351f753f-a2aa-4d7c-8d86-07fa60ab3214" />
+    <projectItemReference itemId="13cceeae-bcda-47c8-ade0-c8b77fa6487a" />
+    <projectItemReference itemId="40cce393-e198-4f00-a140-18677ac7c232" />
+    <projectItemReference itemId="c84b0f8a-7221-4219-ba41-8cc6d693d71a" />
+  </projectItems>
+</feature>
